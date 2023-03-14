07860736032d514a528a5df33d3ce0de0a8bef4c
Juanvi Quiles <juquiles@aidacanarias.com>
2023-03-14 13:00:15 +0100

    feat: project scaffolding

---
+++ StringCalculatorKata/StringCalculatorKata.Domain.Tests/StringCalculatorShould.cs
@@ -0,0 +1,16 @@
+namespace StringCalculatorKata.Domain.Tests
+{
+    public class StringCalculatorShould
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
