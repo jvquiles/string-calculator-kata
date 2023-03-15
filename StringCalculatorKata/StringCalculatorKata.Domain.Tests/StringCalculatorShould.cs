8fa1852e74b6652edc04d7278f2e5f1f59275f1e
Juanvi Quiles <juquiles@aidacanarias.com>
2023-03-15 10:02:24 +0100

    test: Implement add two numbers test case

--- StringCalculatorKata/StringCalculatorKata.Domain.Tests/StringCalculatorShould.cs
+++ StringCalculatorKata/StringCalculatorKata.Domain.Tests/StringCalculatorShould.cs
@@ -16,10 +16,18 @@ namespace StringCalculatorKata.Domain.Tests
         }
 
         [Test]
-        public void SumOne()
+        public void SumOneNumber()
         {
             var stringCalculator = new StringCalculator();
             var result = stringCalculator.Add("1");
+            Assert.That(result, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void SumTwoNumbers()
+        {
+            var stringCalculator = new StringCalculator();
+            var result = stringCalculator.Add("1,2");
             Assert.That(result, Is.EqualTo(1));
         }
     }
