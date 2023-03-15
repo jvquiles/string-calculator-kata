de39dc4d8ff57db0e5834e4d6499eeacfe47cf26
Juanvi Quiles <juquiles@aidacanarias.com>
2023-03-15 13:05:27 +0100

    test: Implement compsed separator test

--- StringCalculatorKata/StringCalculatorKata.Domain.Tests/StringCalculatorShould.cs
+++ StringCalculatorKata/StringCalculatorKata.Domain.Tests/StringCalculatorShould.cs
@@ -118,5 +118,13 @@ namespace StringCalculatorKata.Domain.Tests
             var result = stringCalculator.Add("//;\n1;2;1001;2000");
             Assert.That(result, Is.EqualTo(3));
         }
+
+        [Test]
+        public void AddUsesComposedSeparator()
+        {
+            var stringCalculator = new StringCalculator();
+            var result = stringCalculator.Add("//[***]\n1***2***3");
+            Assert.That(result, Is.EqualTo(6));
+        }
     }
 }
