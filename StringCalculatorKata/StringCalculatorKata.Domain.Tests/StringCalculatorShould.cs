ae6d442548bae8992a96404f873a5b6f09859ad5
Juanvi Quiles <juquiles@aidacanarias.com>
2023-03-15 11:25:55 +0100

    test: add specific separator test case with three numbers

--- StringCalculatorKata/StringCalculatorKata.Domain.Tests/StringCalculatorShould.cs
+++ StringCalculatorKata/StringCalculatorKata.Domain.Tests/StringCalculatorShould.cs
@@ -62,5 +62,13 @@ namespace StringCalculatorKata.Domain.Tests
             var result = stringCalculator.Add("//;\n1;2");
             Assert.That(result, Is.EqualTo(3));
         }
+
+        [Test]
+        public void AddNumbersSpecificSeparatorWithThreeNumbers()
+        {
+            var stringCalculator = new StringCalculator();
+            var result = stringCalculator.Add("//;\n1;2;3");
+            Assert.That(result, Is.EqualTo(10));
+        }
     }
 }
